376012a484bd1e7d77dc5088f6eb02430c35b6ae
Abdelrahman H. Atta <abdelrahmanhossam2222@gmail.com>
2025-10-28 17:49:01 +0300

    Add Db Sets

--- Ticket Management System/Data/TicketContext.cs
+++ Ticket Management System/Data/TicketContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Ticket_Management_System.Models;
 
 namespace Ticket_Management_System.Data
 {
@@ -7,6 +8,17 @@ namespace Ticket_Management_System.Data
         public TicketContext(DbContextOptions<TicketContext> options) : base(options)
         {
         }
+
+        public DbSet<Ticket> Tickets { get; set; }
+        public DbSet<TicketCategory> TicketCategories { get; set; }
+        public DbSet<TicketComment> TicketComments { get; set; }
+        public DbSet<SupportAgent> SupportAgents { get; set; }
+        public DbSet<TicketHistory> TicketHistories { get; set; }
+        public DbSet<TicketPriority> TicketPrioritys { get; set; }
+        public DbSet<TicketStatus> TicketStatuses { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+
 
     }
 }
--- Ticket Management System/Program.cs
+++ Ticket Management System/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using Ticket_Management_System.Data;
+
 namespace Ticket_Management_System
 {
     public class Program
@@ -11,8 +15,17 @@ namespace Ticket_Management_System
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(options =>
+            {
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                options.IncludeXmlComments(xmlPath);
+            });
+
+            builder.Services.AddDbContext<TicketContext>(opt => opt.UseSqlServer(
+                builder.Configuration.GetConnectionString("DefaultConnection")));
 
             var app = builder.Build();
 
