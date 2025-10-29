33804b485d1fec702e360957dd66e2c0d414ec1f
Abdelrahman Hossam Eldin Zaki Ahmed <67892553+Abdelrahman-2222@users.noreply.github.com>
2025-10-29 16:22:42 +0300

    Add Employee Services And Controllers (#1)
    Co-authored-by: Nahla Hesham <102486469+nahlaheshammostafa@users.noreply.github.com>

---
+++ Ticket Management System/DTOs/EmployeeDTO/EmployeeRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Ticket_Management_System.DTOs.DepartmentDTO;
+
+namespace Ticket_Management_System.DTOs.EmployeeDTO
+{
+    public class EmployeeRequestDTO
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public DepartmentResponseDTO Department { get; set; }
+    }
+}
