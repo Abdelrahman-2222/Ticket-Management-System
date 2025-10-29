eae1c96e516310af28a8608736c2854a390323a0
Nahla Hesham <102486469+nahlaheshammostafa@users.noreply.github.com>
2025-10-29 14:54:04 +0200

    Add Employee Services And Controllers

--- Ticket Management System/Contracts/IEmployeeService.cs
+++ Ticket Management System/Contracts/IEmployeeService.cs
@@ -1,7 +1,17 @@
-﻿namespace Ticket_Management_System.Contracts
+﻿using System.Threading.Tasks;
+using Ticket_Management_System.DTOs.EmployeeDTO;
+using Ticket_Management_System.DTOs.TicketDTO;
+
+namespace Ticket_Management_System.Contracts
 
 {
     public interface IEmployeeService
     {
+        Task<EmployeeResponseDTO> CreateEmployeeAsync(EmployeeRequestDTO employeeRequestDTO);
+        Task<EmployeeResponseDTO> GetEmployeeByIdAsync(int id);
+        Task<List<EmployeeResponseDTO>> GetAllEmployeesAsync();
+        Task<EmployeeResponseDTO> UpdateEmployeeAsync(int id, EmployeeRequestDTO employeeRequestDTO);
+        Task<string> DeleteEmployeeAsync(int id);
+
     }
 }
