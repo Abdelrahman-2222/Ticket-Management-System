fac82bb8cbe4559a2d8a1e335e3e11e3c3838383
Hossam <Hossam@mail.com>
2025-10-30 11:26:40 +0300

    TicketComment Service commpleted successfully

---
+++ Ticket Management System/DTOs/TicketCommentDTO/TicketCommentRequestDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Ticket_Management_System.DTOs.TicketCommentDTO
+{
+    public class TicketCommentRequestDTO
+    {
+        public int TicketId { get; set; }
+        public string Content { get; set; }
+        public string AuthorName { get; set; }
+    }
+}
