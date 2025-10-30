5c0cbc6d617ae9f998fb221e40f5bd4eca7d9f92
Abdelrahman Hossam Eldin Zaki Ahmed <67892553+Abdelrahman-2222@users.noreply.github.com>
2025-10-30 11:45:33 +0300

    Dev (#5)
    * Add Employee Services And Controllers
    
    * SupportAgent Service Completed Successfully
    
    * TicketCategory Service Completed Successfully
    
    * TicketComment Service commpleted successfully
    
    * Add Department Service And Controller
    
    ---------
    
    Co-authored-by: Nahla Hesham <102486469+nahlaheshammostafa@users.noreply.github.com>
    Co-authored-by: Hossam <Hossam@mail.com>

---
+++ Ticket Management System/Controllers/DepartmentController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ticket_Management_System.Contracts;
+using Ticket_Management_System.DTOs.DepartmentDTO;
+using Ticket_Management_System.DTOs.EmployeeDTO;
+using Ticket_Management_System.Services;
+
+namespace Ticket_Management_System.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentController : ControllerBase
+    {
+        private readonly IDepartmentService _departmentService;
+        public DepartmentController(IDepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<DepartmentResponseDTO>> CreateDepartment(DepartmentRequestDTO departmentRequestDTO)
+        {
+            var createdDepartment = await _departmentService.CreateDepartmentAsync(departmentRequestDTO);
+            if (createdDepartment == null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Error creating department.");
+            }
+            return CreatedAtAction(nameof(CreateDepartment), new { id = createdDepartment.Id }, createdDepartment);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DepartmentResponseDTO>> GetDepartmentById(int id)
+        {
+            var department = await _departmentService.GetDepartmentByIdAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return Ok(department);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<DepartmentResponseDTO>>> GetAllDepartments()
+        {
+            var departments = await _departmentService.GetAllDepartmentsAsync();
+            if (departments == null || !departments.Any())
+            {
+                return NotFound();
+            }
+            return Ok(departments);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<DepartmentResponseDTO>> UpdateDepartment(int id, DepartmentRequestDTO departmentRequestDTO)
+        {
+            var updatedDepartment = await _departmentService.UpdateDepartmentAsync(id, departmentRequestDTO);
+            if (updatedDepartment == null)
+            {
+                return NotFound();
+            }
+            return Ok(updatedDepartment);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<string>> DeleteDepartment(int id)
+        {
+            var result = await _departmentService.DeleteDepartmentAsync(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+    }
+}
--- Ticket Management System/DTOs/DepartmentDTO/DepartmentResponseDTO.cs
+++ Ticket Management System/DTOs/DepartmentDTO/DepartmentResponseDTO.cs
@@ -1,8 +1,12 @@
-﻿namespace Ticket_Management_System.DTOs.DepartmentDTO
+﻿using Ticket_Management_System.DTOs.EmployeeDTO;
+using Ticket_Management_System.Models;
+
+namespace Ticket_Management_System.DTOs.DepartmentDTO
 {
     public class DepartmentResponseDTO
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public ICollection<EmployeeGetResponseDTO> Employees { get; set; }
     }
 }
