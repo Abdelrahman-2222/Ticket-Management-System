53866f9b2d27036a6201e4507f748d3d09c37fa0
Abdelrahman Hossam Eldin Zaki Ahmed <67892553+Abdelrahman-2222@users.noreply.github.com>
2025-10-30 11:33:13 +0300

    Dev (#4)
    * Add Employee Services And Controllers
    
    * SupportAgent Service Completed Successfully
    
    * TicketCategory Service Completed Successfully
    
    * TicketComment Service commpleted successfully
    
    ---------
    
    Co-authored-by: Nahla Hesham <102486469+nahlaheshammostafa@users.noreply.github.com>
    Co-authored-by: Hossam <Hossam@mail.com>

--- Ticket Management System/Contracts/ITicketCommandsService.cs
+++ Ticket Management System/Contracts/ITicketCommandsService.cs
@@ -1,6 +1,14 @@
-﻿namespace Ticket_Management_System.Contracts
+﻿using Ticket_Management_System.DTOs.TicketCommentDTO;
+
+namespace Ticket_Management_System.Contracts
 {
     public interface ITicketCommandsService
     {
+        Task<List<TicketCommentResponseDTO>> GetAllTicketCommentAsync();
+        Task<TicketCommentResponseDTO> GetTicketCommentByIdAsync(int id);
+        Task<TicketCommentResponseDTO> CreateTicketCommentAsync(TicketCommentRequestDTO ticketCommentRequestDTO);
+        Task<TicketCommentResponseDTO> UpdateTicketCommentAsync(int id, TicketCommentRequestDTO ticketCommentRequestDTO);
+        Task<string> DeleteTicketCommentAsync(int id);
+        Task SaveChangesAsync();
     }
 }
