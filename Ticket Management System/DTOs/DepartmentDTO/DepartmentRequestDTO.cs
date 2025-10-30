063ba2d50b0a47ace6e30b203b5b42fe88d82bfa
Nahla Hesham <102486469+nahlaheshammostafa@users.noreply.github.com>
2025-10-30 10:35:55 +0200

    Add Department Service And Controller

---
+++ Ticket Management System/DTOs/DepartmentDTO/DepartmentRequestDTO.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ticket_Management_System.DTOs.DepartmentDTO
+{
+    public class DepartmentRequestDTO
+    {
+        [Required]
+        public string Name { get; set; }
+    }
+}
